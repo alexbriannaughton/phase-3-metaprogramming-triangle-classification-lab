f548f0bb0fcbdd78709673a19c73ef2c8ff0cbce
Alex Brian Naughton <alexbriannaughton@gmail.com>
2022-11-02 20:28:07 -0700

    commit tree 74bd992a8c1599252a212f83abbab6d9e6a4e7d5

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a=a
+    @b=b
+    @c=c
+  end
+
+  def kind
+    if a == b && b == c
+      :equilateral
+    elsif a == b || a == c || b == c
+      :isosceles
+    elsif a != b && b != c && a != c
+      :scalene
+    elsif a <= 0 || b <= 0 || c <= 0
+      raise TriangleError
+    elsif a + b > c || a + c > b || b + c > a
+      raise TriangleError
+    end  
+  end
+
+  class TriangleError < StandardError
+    "dang"
+  end
+
+
 end
